6bd7e59f7e768557ae4a78d47ed5764bee0a34b3
IvailoAtsv <ivailoatanassovv@gmail.com>
2023-06-13 10:22:28 +0300

    add database
    setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const path = require('path')
 const routes = require('./routes')
-
+const dbConnect = require('./config/dbConfig')
 const app = express()
-const PORT = 3000
 
+const PORT = 3000
 require('./config/expressConfig')(app)
 require('./config/hbsConfig')(app)
+
+dbConnect()
+    .then(() => console.log('db connected successfuly'))
+    .catch(err => console.log(err))
 
 app.use(routes)
 
