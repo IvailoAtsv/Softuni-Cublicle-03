aefa158e48b93978521362b358af3bedb12a7b40
IvailoAtsv <ivailoatanassovv@gmail.com>
2023-06-14 15:27:51 +0300

    add post action for accessories

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,15 @@
+const { log } = require('console')
+
 const router = require('express').Router()
 
 router.get('/create', (req, res) => {
     res.render('accessories/create')
 })
 
+router.post('/create', (req, res) => {
+    const body = req.body
+    log(body)
+    res.redirect('/')
+})
 
 module.exports = router
