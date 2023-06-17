71d2ed475007bc7feacae5e04a362a2c7bc1c026
IvailoAtsv <ivailoatanassovv@gmail.com>
2023-06-17 09:36:18 +0300

    added user login

--- src/models/User.js
+++ src/models/User.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: {
-        type: String,
-        // validate:{
-        //     validator: function(value){
-        //         return this.repeatPassword === value
-        //     },
-        //     message: 'Passwords don\'t match!'
-        // }   
+    password: String,
+})
+
+userSchema.virtual('repeatPassword').set(function (value) {
+    if (value != this.password) {
+        throw new mongoose.MongooseError('Passwords don\'t match')
     }
 })
 
-userSchema.virtual('repeatPassword').set(function(value){
-if(value!= this.password){
-    throw new mongoose.MongooseError('Passwords don\'t match')
-}
+userSchema.pre('save', function () {
+    const hash = bcrypt.hash(this.password, 10)
+
+    this.password = hash
 })
 
 const User = mongoose.model('User', userSchema)
--- src/services/userManager.js
+++ src/services/userManager.js
@@ -1,3 +1,21 @@
+const { default: mongoose } = require('mongoose')
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
 exports.register = (userData) => User.create(userData)
+
+exports.login = async (username, password) => {
+    //todo check if user exists
+    const user = await User.findOne({ username })
+
+    if(!user){
+        throw new Error('Cannot find username or password')
+    }
+    //validate password
+    const isValid = await bcrypt.compare(password, user.password)
+    if(!isValid){
+        throw new Error('Cannot find username or password')
+    }
+    //return user
+    return user
+}
